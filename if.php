e61589a5a9511a4b7ee619ac6517a01614f903b9
Julie Buser <juliebuser1@Julies-MacBook-Pro.local>
2015-07-13 10:46:24 -0500

    added if exercise

--- if.php
+++ if.php
@@ -1,7 +1,7 @@
 <?php
 
 $a = 5;
-$b = 10;
+$b = '10';
 $c = '10';
 
  if ($a < $b) {
@@ -43,6 +43,52 @@ $c = '10';
  if ($b !== $c) {
      echo "$b is not identical to $c\n";
  }
+
+ // Shorten the next 2 statements into an if/else
+ if ($a < $b) {
+     echo "$a is less than $b";
+ } else {
+     echo "$a is greater than $b";
+ }
+
+ if ($a > $b) {
+     echo "$b is greater than $a";
+ } else {
+     echo "$b is less than $a";
+ }
+
+ // Shorten the next 2 statements into an if/else
+ if ($b >= $c) {
+     // output the appropriate result
+     echo "$b is greater than or equal to $c\n";
+ } else {
+     echo "$b is less than $c\n";
+ }
+
+ if ($b <= $c) {
+     // output the appropriate result
+     echo "$b is less than or equal to $c\n";
+ } else {
+     echo "$b is greater than $c";
+ }
+
+ // combine the next 4 conditionals into
+ // one if/else/elseif block that checks in order for:
+ // identical, equal, not identical, not equal
+ 
+if ($b === $c) {
+     // output the appropriate result
+     echo "$b is identical to $c" . PHP_EOL;
+ } elseif ($b == $c) {
+     // output the appropriate result
+     echo "$b is equal to $c" . PHP_EOL;
+ } elseif ($b !== $c) {
+     // output the appropriate result
+     echo "$b is not identical to $c" . PHP_EOL;
+ } else {
+     // output the appropriate result
+     echo "$b is not equal to $c" . PHP_EOL;
+}
 
 
 ?>
