0a9612f9b9bae7663198ccd5231e9b37d18b3df9
Julie Buser <jbuser2013@gmail.com>
2015-07-14 14:48:35 -0500

    updated foreach exercise RE array

--- foreach.php
+++ foreach.php
@@ -3,8 +3,18 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $stuff) {
-	if (is_scalar($stuff)) {
-        echo "{$stuff} is a scalar\n";
+	if (is_integer($stuff)) {
+        echo "{$stuff} is an integer\n";
+    } elseif (is_float($stuff)) {
+        echo "{$stuff} is a floating point number\n";
+    } elseif (is_bool($stuff)) {
+    	echo "{$stuff} is a boolean\n";
+    } elseif (is_array($stuff)) {
+    	echo "This is an array\n";
+    } elseif (is_null($stuff)) {
+    	echo "{$stuff} has null value\n";
+    } elseif (is_string($stuff)) {
+    	echo "{$stuff} is a string\n";
     };
 }
 
