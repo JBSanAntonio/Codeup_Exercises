73e0ad9d1e889315b34d208bab4b10bcc53b1801
Julie Buser <jbuser2013@gmail.com>
2015-07-14 11:41:54 -0500

    increment stage of for.php exercise

--- for.php
+++ for.php
@@ -4,7 +4,9 @@
 		$argv[1] = trim(fgets(STDIN) . PHP_EOL);
 		fwrite(STDOUT, "Please enter an ending number");
 		$argv[2] = trim(fgets(STDIN) . PHP_EOL);
-		for ($i=$argv[1]; $i<=$argv[2]; $i++) {	
+		fwrite(STDOUT, "Select the increment");
+		$argv[3] = trim(fgets(STDIN) . PHP_EOL);
+		for ($i=$argv[1]; $i<=$argv[2]; $i+=$argv[3]) {	
 			echo "\$i has a value of {$i}\n";
 		}
 ?>
