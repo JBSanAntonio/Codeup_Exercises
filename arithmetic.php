979af4096a71b8be305ce090999dd0d854dd36e0
Julie Buser <jbuser2013@gmail.com>
2015-07-15 11:37:17 -0500

    added arithmetic php file

--- arithmetic.php
+++ arithmetic.php
@@ -2,22 +2,46 @@
 
 function add($a, $b)
 {
-    echo $a + $b;
+    return $a + $b;
 }
 
 function subtract($a, $b)
 {
     // Add code here
+    return $a - $b;
 }
 
 function multiply($a, $b)
 {
     // Add code here
+    return $a * $b;
 }
 
 function divide($a, $b)
 {
     // Add code here
+    return $a / $b;
+}
+
+function modulus($a, $b)
+{
+	return $a % $b;
 }
 
 // Add code to test your functions here
+	$number = add(10, 5);
+	echo $number . PHP_EOL;
+
+	$number = subtract(10, 5);
+	echo $number . PHP_EOL;
+
+	$number = multiply(10, 5);
+	echo $number . PHP_EOL;
+
+	$number = divide(10, 5);
+	echo $number . PHP_EOL;
+
+	$number = modulus(10, 5);
+	echo $number . PHP_EOL;
+
+?>
