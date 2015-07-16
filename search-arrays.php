826b28c9d0bba9a280e51802cde9937c7e8bed3a
Julie Buser <jbuser2013@gmail.com>
2015-07-16 13:28:54 -0500

    test whether Tina & Bob are in Array

--- search-arrays.php
+++ search-arrays.php
@@ -1,6 +1,6 @@
 <?php
 
-/*Search for Tina and Bob in $names. Make sure it works as expected.
+/*Search for Bob and Bob in $names. Make sure it works as expected.
 */
 
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
@@ -8,12 +8,19 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 function isInArray($query, $names) {
-	$result = array_search($query, $haystack);
+	$result = array_search($query, $names);
 	if($result !== false) {
 		return true;
 	} else {
 		return false;
 	};
 };
-	
+	var_dump(isInArray('Tina', $names));
+	if(isInArray('Tina', $names)) {
+		echo 'Tina is in the array' . PHP_EOL;
+	}
+	var_dump(isInArray('Bob', $names));
+	if(isInArray('Bob', $names)) {
+		echo 'Bob is in the array' . PHP_EOL;
+	}
 ?>
