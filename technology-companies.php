a5470f6f0e8c8ba5de302ef8e2dc344b488ce337
Julie Buser <jbuser2013@gmail.com>
2015-07-16 11:22:53 -0500

    steps 1 and 2 of exercise

---
+++ technology-companies.php
@@ -0,0 +1,60 @@
+<?php
+
+$companies = [
+    'Sun Microsystems' => [
+        'Vinod Khosla',
+        'Bill Joy',
+        'Andy Bechtolsheim',
+        'Scott McNealy'
+    ],
+    'Silicon Graphics' => [
+        'Jim Clark',
+        'Ed McCracken'
+    ],
+    'Cray' => [
+        'William Norris',
+        'Seymour Cray'
+    ],
+    'NeXT' => [
+        'Steve Jobs',
+        'Avie Tevanian',
+        'Joanna Hoffman',
+        'Bud Tribble',
+        'Susan Kare'
+    ],
+    'Acorn Computers' => [
+        'Steve Furber',
+        'Sophie Wilson',
+        'Hermann Hauser',
+        'Jim Mitchell'
+    ],
+    'MIPS Technologies' => [
+        'Skip Stritter',
+        'John L. Hennessy'
+    ],
+    'Commodore' => [
+        'Yash Terakura',
+        'Bob Russell',
+        'Bob Yannes',
+        'David A. Ziembicki',
+        'Jay Miner'
+    ],
+    'Be Inc' => [
+        'Steve Sakoman',
+        'Jean-Louis Gassée'
+    ]
+];
+/*Sort the $companies array by company name and output the results.
+*/
+    ksort($companies);
+
+/*Sort the people in each company alphabetically. You will need to use a foreach loop and will need to reassign each inner array after sorting. Output the result.
+*/
+    foreach($companies as $companyKey => $peopleVal) { 
+    sort($peopleVal);  
+    $companies[$companyKey] = $peopleVal;
+    }
+    print_r($companies);
+
+
+?>
