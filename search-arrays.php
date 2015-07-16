3daaed43c3d7134cfdd93b2b583d1ec3065fd989
Julie Buser <jbuser2013@gmail.com>
2015-07-16 12:12:32 -0500

    is array test

---
+++ search-arrays.php
@@ -0,0 +1,18 @@
+<?php
+
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+/*Create a function that returns TRUE or FALSE if an array value is found.*/
+
+$names = array('this', 'is', 'an array');
+
+echo is_array($names) ? 'Array' : 'not an Array';
+echo "\n";
+
+$no = 'this is a string';
+
+echo is_array($no) ? 'True' : 'False';
+
+?>
