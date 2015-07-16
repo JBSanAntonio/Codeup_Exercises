059562c96feb1a64ee573f71f4b08d46f815a0e8
Julie Buser <jbuser2013@gmail.com>
2015-07-16 13:24:38 -0500

    stage 1 function is value in array T or F

--- search-arrays.php
+++ search-arrays.php
@@ -1,18 +1,19 @@
 <?php
 
+/*Search for Tina and Bob in $names. Make sure it works as expected.
+*/
+
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-/*Create a function that returns TRUE or FALSE if an array value is found.*/
-
-$names = array('this', 'is', 'an array');
-
-echo is_array($names) ? 'Array' : 'not an Array';
-echo "\n";
-
-$no = 'this is a string';
-
-echo is_array($no) ? 'True' : 'False';
-
+function isInArray($query, $names) {
+	$result = array_search($query, $haystack);
+	if($result !== false) {
+		return true;
+	} else {
+		return false;
+	};
+};
+	
 ?>
