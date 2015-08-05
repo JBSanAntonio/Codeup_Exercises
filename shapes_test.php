abba2aa04ce6e9ca7c5cb087f1fcd9b6b9429e4f
Julie Buser <jbuser2013@gmail.com>
2015-08-05 11:06:40 -0500

    shapes updated

--- shapes_test.php
+++ shapes_test.php
@@ -1,10 +1,32 @@
 <?PHP
 
 require_once 'rectangle.php';
+require_once 'square.php';
 
-
+/*Test your new class by creating an instance of 
+Rectangle with various heights and widths. */
+$rectangle1 = new Rectangle(4, 8);
 /*Calling the area method should correctly 
 display the product of height and width.*/
-echo $rectangle->area();
+echo $rectangle1->area();
+
+$rectangle2 = new Rectangle(6, 12);
+echo $rectangle2->area();
+
+
+/*Test your new Square class by creating an 
+instance of Square with various matching 
+height and width. Calling the area() method 
+should correctly display the product of 
+height and width. Invoking the new method 
+perimeter() should display the perimeter of 
+the square.
+*/
+
+$square1 = new Square(4, 4);
+echo $square1->area();
+
+$square2 = new Square(2, 2);
+echo $square2->perimeter();
 
 ?>
